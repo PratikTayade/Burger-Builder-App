6a9f0213badfd36ab0d7c2f744f67722cd22581f
PratikTayade <69456682+PratikTayade@users.noreply.github.com>
2020-08-24 20:54:28 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Layout from './components/Layout/Layout';
-
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render(){
     return(
       <div>
-        <Layout />
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
       
     )
